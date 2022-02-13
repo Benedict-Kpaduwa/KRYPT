1e79ea85138776fe819e2c731b90c2638212dc76
Benedict Kpaduwa <42049724+Benedict-Kpaduwa@users.noreply.github.com>
2022-02-13 16:25:05 +0100

    Finished

--- Client/src/context/TransactionContext.jsx
+++ Client/src/context/TransactionContext.jsx
@@ -11,7 +11,7 @@ const {ethereum} = window;
 const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum)
     const signer = provider.getSigner();
-    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
+    const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
 
     // console.log({
     //     provider,
@@ -28,9 +28,32 @@ export const TransactionProvider = ({children}) => {
     const [formData, setFormData] = useState({ address: "", amount: "",keyword: "", message:""});
     const [isLoading, setIsLoading] = useState(false);
     const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
+    const [transactions, setTransactions] = useState([]);
 
     const handleChange = (e, name) =>{
         setFormData((prevState) => ({...prevState, [name]: e.target.value}));
+    }
+
+    const getAllTransactions = async () => {
+        try {
+            if(!ethereum) return alert("Please install metamask");
+            const transactionContract = getEthereumContract();
+
+            const availableTransactions = await transactionContract.getAllTransactions();
+            const structuredTransactions = availableTransactions.map((transaction)=> ({
+                addressTo: transaction.receiver,
+                addressFrom: transaction.sender,
+                timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+                message: transaction.message,
+                keyword: transaction.keyword,
+                amount: parseInt(transaction.amount._hex)/(10 ** 18)
+            }))
+
+            setTransactions(structuredTransactions);
+            console.log(structuredTransactions);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const checkIfWalletIsConnected = async () => {
@@ -44,6 +67,7 @@ export const TransactionProvider = ({children}) => {
                 setCurrentAccount(accounts[0]);
 
                 //To get all transactions
+                getAllTransactions();
             }else{
                 console.log("No accounts found");    
             }
@@ -51,6 +75,19 @@ export const TransactionProvider = ({children}) => {
         }catch(error){
             console.log(error);
             throw new Error("No ethereum object.")
+        }
+    }
+
+    const checkIfTransactionsExist = async () => {
+        try {
+            const transactionContract = getEthereumContract();
+            const transactionCount = await transactionContract.getTransactionCount();
+
+            window.localStorage.setItem("transactionCount", transactionCount)
+        } catch (error) {
+            console.log(error);
+
+            throw new Error("No ethereum object. ");
         }
     }
 
@@ -63,7 +100,7 @@ export const TransactionProvider = ({children}) => {
         }catch(error){
             console.log(error);
 
-            throw new Error("No ethereum object. ")
+            throw new Error("No ethereum object. ");
         }
     }
 
@@ -97,6 +134,7 @@ export const TransactionProvider = ({children}) => {
             const transactionCount = await transactionContract.getTransactionCount();
 
             setTransactionCount(transactionCount.toNumber());
+            window.reload();
         } catch(error){
             console.log(error);
             throw new Error("No ethereum object. ");
@@ -105,10 +143,11 @@ export const TransactionProvider = ({children}) => {
 
     useEffect(()=>{
         checkIfWalletIsConnected();
+        checkIfTransactionsExist();
     }, []);
 
     return (
-        <TransactionContext.Provider value={{connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction}}>
+        <TransactionContext.Provider value={{connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction, transactions,isLoading}}>
             {children}
         </TransactionContext.Provider>
     )
